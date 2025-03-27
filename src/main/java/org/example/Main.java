d1d3a16e145f4af386d852f39029d4883225bcaa
Eric Schmidt <eric0802@web.de>
2025-03-27 09:36:57 +0100

    add methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static String toUpper(String s) {
+        return s.toUpperCase();
+    }
+
+    public static boolean isPositive(int a) {
+        return a > 0;
+    }
 }
